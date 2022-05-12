c789580378449429728331db32294d69680ce202
ShagalovNick <sekunda-61@yandex.ru>
2022-05-12 21:18:18 +0300

    fix autoload

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -2,6 +2,14 @@
 
 namespace Differ\Differ;
 
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
 use Symfony\Component\Yaml\Yaml;
 use Hexlet\Code\Formatters;
 use Hexlet\Code\Formatters\Stylish;
