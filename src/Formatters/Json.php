d17eca1264a8bddcde3757e33d038656c995a48f
ShagalovNick <sekunda-61@yandex.ru>
2022-05-13 11:42:53 +0300

    fix linter

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -7,14 +7,14 @@ use function Functional\sort;
 function getArrJson(array $dif)
 {
     ksort($dif); //7
-    array_map(function($key, $value) use (&$result) {
+    array_map(function ($key, $value) use (&$result) {
         if (!isset($value['old']) && !isset($value['new']) && !isset($value['nodif'])) {
             if (is_array($value)) {
                 $result [$key] = getArrJson($value);
             }
         }
         $arFormat = ['old' => "- ", 'new' => "+ ", 'nodif' => ""];
-        array_map(function($arkey, $arvalue) use (&$result, $value, $key) {
+        array_map(function ($arkey, $arvalue) use (&$result, $value, $key) {
             if (isset($value[$arkey])) {
                 $result[$arvalue . $key] = $value[$arkey];
             }
@@ -28,8 +28,8 @@ function json(array $dif)
     $result = getArrJson($dif);
     if (file_put_contents("chart_data.json", json_encode($result, JSON_UNESCAPED_UNICODE))) {
         echo json_encode($result, JSON_PRETTY_PRINT);
-    $resultStr = json_encode($result, JSON_PRETTY_PRINT);
-    return $resultStr;
+        $resultStr = json_encode($result, JSON_PRETTY_PRINT);
+        return $resultStr;
     } else {
         echo 'Error';
         return false;
